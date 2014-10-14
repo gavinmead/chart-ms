e072393fb74a636fbbe626348e390a75826fba7c
Gavin Mead <gavin.w.mead@gmail.com>
2014-10-14 16:11:47 -0400

    Chart creation work.

--- src/main/java/io/gmi/chartms/api/ChartController.java
+++ src/main/java/io/gmi/chartms/api/ChartController.java
@@ -1,22 +1,35 @@
 package io.gmi.chartms.api;
 
+import com.google.common.io.Files;
+import io.gmi.chartms.service.ChartCreationContext;
+import io.gmi.chartms.service.ChartCreationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 
 /**
  * Created by gmead on 10/14/14.
  */
+@RestController
 @RequestMapping(value = UrlConstants.CHART)
 public class ChartController {
   private static final Logger log = LoggerFactory.getLogger(ChartController.class);
 
-  @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.IMAGE_PNG_VALUE)
-  public byte[] createChart(@RequestBody CreateChartRequest request) {
+  @Autowired
+  ChartCreationService chartCreationService;
 
-    return null;
+  @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.IMAGE_PNG_VALUE)
+  public byte[] createChart(@RequestBody CreateChartRequest request, HttpServletRequest httpServletRequest) throws Exception {
+    ChartCreationContext context = new ChartCreationContext((String) httpServletRequest.getAttribute(ContextIDInterceptor.CONTEXT_ID_KEY), request);
+    String savedImage = chartCreationService.createChartImage(context);
+    return Files.toByteArray(new File(savedImage));
   }
 }
---
+++ src/test/java/io/gmi/chartms/api/ChartControllerTest.java
@@ -0,0 +1,84 @@
+package io.gmi.chartms.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
+import com.google.common.io.ByteSource;
+import com.google.common.io.Files;
+import io.gmi.chartms.Application;
+import io.gmi.chartms.TestUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.*;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@IntegrationTest("server.port=0")
+public class ChartControllerTest {
+
+  private static final Logger log = LoggerFactory.getLogger(ChartControllerTest.class);
+
+  private static final String FORMAT = "http://localhost:%d/api/chart";
+
+  RestTemplate restTemplate = new TestRestTemplate();
+
+  @Autowired
+  ResourceLoader resourceLoader;
+
+  @Value("${local.server.port}")
+  int port;
+
+  @Autowired
+  ObjectMapper objectMapper;
+
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @Test
+  public void testCreateChart() throws Exception {
+    CreateChartRequest request = new CreateChartRequest();
+    request.setHtml(TestUtils.HTML);
+    request.setChart(TestUtils.CHART);
+    request.setUseBootstrap(true);
+    request.setClippingDiv("test");
+    request.setChartContainerId("chart");
+
+    String toJson = objectMapper.writeValueAsString(request);
+
+    //Sanity check
+    CreateChartRequest fromJson = objectMapper.readValue(toJson, CreateChartRequest.class);
+
+    log.info("JSON Request: \n{}\n", toJson);
+    String url = String.format(FORMAT, port);
+    HttpHeaders requestHeaders = new HttpHeaders();
+    requestHeaders.setContentType(MediaType.APPLICATION_JSON);
+    requestHeaders.setAccept(Lists.newArrayList(MediaType.IMAGE_PNG));
+    HttpEntity<String> requestEntity = new HttpEntity<>(toJson, requestHeaders);
+    ResponseEntity<byte[]> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, byte[].class);
+
+    ByteSource savedImageByteSource = ByteSource.wrap(responseEntity.getBody());
+    Resource expected = resourceLoader.getResource("classpath:/expected.png");
+    ByteSource expectedByteSource = Files.asByteSource(expected.getFile());
+
+    assertThat(expectedByteSource.contentEquals(savedImageByteSource)).isTrue();
+  }
+}
